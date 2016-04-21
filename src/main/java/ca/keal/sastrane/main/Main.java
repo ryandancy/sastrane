f8a882643b9949089077be203c3cc042395b3a13
Copper <git@keal.ca>
2016-04-21 06:45:17 -0400

    Fixed path to FXML in Main.

--- src/main/java/ca/keal/sastrane/main/Main.java
+++ src/main/java/ca/keal/sastrane/main/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.net.URL;
+
 public class Main extends Application {
     
     public static void main(String[] args) {
@@ -15,8 +17,14 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         // TODO extract into utility method
-        Parent root = FXMLLoader.load(getClass().getResource("src/main/resources/ca/keal/sastrane/gui/main-menu.fxml"));
-        Scene mainMenu = new Scene(root, 400, 400); // TODO stop hard-coding dimensions as 400x400, dynamic title?
+        URL fxml = getClass().getClassLoader().getResource("ca/keal/sastrane/gui/main-menu.fxml");
+        if (fxml == null) {
+            // this shouldn't happen
+            throw new RuntimeException("ca/keal/sastrane/gui/main-menu.fxml could not be found");
+        }
+        
+        Parent root = FXMLLoader.load(fxml);
+        Scene mainMenu = new Scene(root, 400, 400); // TODO stop hard-coding dimensions as 400x400; dynamic title?
         primaryStage.setTitle("Sastrane");
         primaryStage.setScene(mainMenu);
         primaryStage.show();
