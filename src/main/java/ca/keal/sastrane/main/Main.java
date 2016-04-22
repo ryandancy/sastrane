bc9f44a3bbe886bcf1025e1787daf13892f2f2a9
Copper <git@keal.ca>
2016-04-22 06:40:48 -0400

    Fixed lombok messing up the RuleSet discovery.

--- src/main/java/ca/keal/sastrane/main/Main.java
+++ src/main/java/ca/keal/sastrane/main/Main.java
@@ -6,6 +6,8 @@ import com.google.common.reflect.ClassPath;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.lang.reflect.Modifier;
+
 public class Main extends Application {
     
     public static void main(String[] args) {
@@ -14,12 +16,18 @@ public class Main extends Application {
     
     @Override
     public void start(Stage primaryStage) throws Exception {
+        // TODO splash screen
+        
         // Load all classes, find all subclasses of RuleSet, register each
         for (ClassPath.ClassInfo classInfo : ClassPath.from(getClass().getClassLoader()).getAllClasses()) {
-            Class<?> cls = classInfo.load();
-            if (RuleSet.class.isAssignableFrom(cls)) {
-                @SuppressWarnings("unchecked") RuleSet ruleSet = ((Class<? extends RuleSet>) cls).newInstance();
-                RuleSet.registerRuleSet(ruleSet);
+            // Exclude lombok, guava
+            String pkg = classInfo.getPackageName();
+            if (!pkg.startsWith("lombok") && !pkg.startsWith("com.google.common")) {
+                Class<?> cls = classInfo.load();
+                if (RuleSet.class.isAssignableFrom(cls) && !Modifier.isAbstract(cls.getModifiers())) {
+                    @SuppressWarnings("unchecked") RuleSet ruleSet = ((Class<? extends RuleSet>) cls).newInstance();
+                    RuleSet.registerRuleSet(ruleSet);
+                }
             }
         }
         
