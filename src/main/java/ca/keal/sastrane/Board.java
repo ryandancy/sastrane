1384d3fb5a527ff91145aeb6dfa0a23551354faf
Copper <git@keal.ca>
2016-04-23 07:03:27 -0400

    Added lazy maxx and maxy getters to Board.

--- src/main/java/ca/keal/sastrane/Board.java
+++ src/main/java/ca/keal/sastrane/Board.java
@@ -3,14 +3,18 @@ package ca.keal.sastrane;
 import ca.keal.sastrane.util.Pair;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Singular;
 import lombok.ToString;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @ToString
 @EqualsAndHashCode
@@ -19,6 +23,12 @@ public class Board {
     
     @NonNull
     private final Map<Square, Pair<Piece, Player>> squaresToPieces; // TODO more dimensions?
+    
+    @Getter(lazy = true)
+    private final int maxX = getMaxDimen(Square::getX);
+    
+    @Getter(lazy = true)
+    private final int maxY = getMaxDimen(Square::getY);
     
     /**
      * ' ' = null piece, '_' = not on board, is error if either used in pieces.
@@ -56,6 +66,13 @@ public class Board {
     
     public boolean isOn(Square square) {
         return squaresToPieces.containsKey(square);
+    }
+    
+    /**
+     * Pass Square::getX for maxX, or Square::getY for maxY
+     */
+    private int getMaxDimen(Function<Square, Integer> map) {
+        return Collections.min(squaresToPieces.keySet().stream().map(map).collect(Collectors.toList()));
     }
     
 }
