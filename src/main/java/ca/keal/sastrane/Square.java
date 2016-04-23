c8a97111af279e2cc57e5187d20df195fa99190e
Copper <git@keal.ca>
2016-04-23 14:06:05 -0400

    Added withers to Square.

--- src/main/java/ca/keal/sastrane/Square.java
+++ src/main/java/ca/keal/sastrane/Square.java
@@ -2,8 +2,10 @@ package ca.keal.sastrane;
 
 import lombok.NonNull;
 import lombok.Value;
+import lombok.experimental.Wither;
 
 @Value
+@Wither
 public final class Square {
     
     private final int x;
