ee25324219062762bb0d559ec798ce6351dafc4a
Copper <git@keal.ca>
2016-04-23 07:41:20 -0400

    Added perspective change method to Player, implemented in ChessPlayer.

--- src/main/java/ca/keal/sastrane/Player.java
+++ src/main/java/ca/keal/sastrane/Player.java
@@ -11,17 +11,24 @@ import lombok.NonNull;
  * <pre><code>
  *     public enum FooPlayer implements Player {
  *         
- *         PLAYERA("a"), PLAYERB("b");
+ *         PLAYERA("a", Player::flipNone), PLAYERB("b", Player::flipY);
  *         
  *         private String name;
+ *         private BinaryOperator&lt;Square&gt; perspectiveChanger;
  *         
- *         FooPlayer(String name) {
+ *         FooPlayer(String name, BinaryOperator&lt;Square&gt; perspectiveChanger) {
  *             this.name = name;
+ *             this.perspectiveChanger = perspectiveChanger;
  *         }
  *         
  *        {@literal @}Override
  *         public String getName() {
  *             return name;
+ *         }
+ *         
+ *        {@literal @}Override
+ *         public Square toPerspective(Square pos, Square original) {
+ *             return perspectiveChanger.apply(pos, original);
  *         }
  *         
  *     }
@@ -33,5 +40,50 @@ public interface Player {
     
     @NonNull
     String getName();
+    
+    /**
+     * Returns the square from this player's "perspective".
+     * <p />
+     * {@link Piece#getPossibleMoves} returns how the piece would move, assuming that the piece is
+     * facing forwards (+y). However, for some players, this is not the case. For example, in chess, white is facing
+     * forwards (+y) and black is facing towards the back of the board (-y). A black pawn will return the square
+     * [x, y+1]; however, this would have it move towards its own side! To counteract this, {@code toPerspective}
+     * is called on each result, and the black pawn now moves towards white's side.
+     * <p />
+     * Multiple static utility functions are provided as implementations of toPerspective: {@link #flipBoth},
+     * {@link #flipX}, {@link #flipY}, and {@link #flipNone}.
+     * 
+     * @param pos The base position in question; most implementations will "flip" over this.
+     * @param original The original square being looked at from another perspective.
+     * @return {@code original} from this perspective.
+     * @see #flipBoth(Square, Square)
+     * @see #flipX(Square, Square)
+     * @see #flipY(Square, Square)
+     * @see #flipNone(Square, Square)
+     */
+    // TODO some form of full-board toPerspective
+    @NonNull
+    Square toPerspective(@NonNull Square pos, @NonNull Square original);
+    
+    @NonNull
+    static Square flipNone(@SuppressWarnings("UnusedParameters") @NonNull Square pos, @NonNull Square original) {
+        return original;
+    }
+    
+    // DRY violated???
+    @NonNull
+    static Square flipX(@NonNull Square pos, @NonNull Square original) {
+        return new Square(pos.getX() - (pos.getX() - original.getX()), original.getY());
+    }
+    
+    @NonNull
+    static Square flipY(@NonNull Square pos, @NonNull Square original) {
+        return new Square(original.getX(), pos.getY() - (pos.getY() - original.getY()));
+    }
+    
+    @NonNull
+    static Square flipBoth(@NonNull Square pos, @NonNull Square original) {
+        return new Square(pos.getX() - (pos.getX() - original.getX()), pos.getY() - (pos.getY() - original.getY()));
+    }
     
 }
--- src/main/java/ca/keal/sastrane/chess/ChessPlayer.java
+++ src/main/java/ca/keal/sastrane/chess/ChessPlayer.java
@@ -1,15 +1,26 @@
 package ca.keal.sastrane.chess;
 
 import ca.keal.sastrane.Player;
+import ca.keal.sastrane.Square;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
-@Getter
+import java.util.function.BinaryOperator;
+
 @RequiredArgsConstructor
 public enum ChessPlayer implements Player {
     
-    WHITE("white"), BLACK("black");
+    WHITE("white", Player::flipNone), BLACK("black", Player::flipY);
     
+    @Getter
     private final String name;
+    private final BinaryOperator<Square> perspectivizer;
+    
+    @Override
+    @NonNull
+    public Square toPerspective(@NonNull Square pos, @NonNull Square original) {
+        return perspectivizer.apply(pos, original);
+    }
     
 }
