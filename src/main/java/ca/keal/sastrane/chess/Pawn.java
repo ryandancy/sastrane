40865f2d88eda644a338c9b28151ec3ee2b3a1b3
Copper <git@keal.ca>
2016-04-23 15:03:01 -0400

    Added Pawn.

---
+++ src/main/java/ca/keal/sastrane/chess/Pawn.java
@@ -0,0 +1,85 @@
+package ca.keal.sastrane.chess;
+
+import ca.keal.sastrane.Board;
+import ca.keal.sastrane.Move;
+import ca.keal.sastrane.Piece;
+import ca.keal.sastrane.Player;
+import ca.keal.sastrane.Round;
+import ca.keal.sastrane.Square;
+import ca.keal.sastrane.util.Pair;
+import ca.keal.sastrane.util.Utils;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class Pawn implements Piece {
+    
+    // TODO: add MoveEvent event handler, check for double step by pawn, set, reset, hasMoved, promotion
+    private boolean lastMoveDouble;
+    private boolean moved;
+    
+    @Override
+    @NonNull
+    public List<Move> getPossibleMoves(@NonNull Round round, @NonNull Square boardPos, @NonNull Player allegiance) {
+        List<Move> moves = new ArrayList<>();
+        
+        // One in front if not occupied; two in front if first and not occupied
+        for (int n = 1; n <= (moved ? 1 : 2); n++) {
+            Square nInFront = boardPos.withY(boardPos.getY() + n);
+            // round.getBoard().isOn(nInFront) shouldn't be possible b/c promotions... handle as special case???
+            if (round.getBoard().isOn(nInFront) && round.getBoard().get(nInFront) == null) {
+                moves.add(boardPos.to(nInFront));
+            }
+        }
+        
+        // Diagonally if occupied by opposite player
+        for (int i = -1; i <= 1; i += 2) {
+            Square diagonal = new Square(boardPos.getX() + i, boardPos.getY() + 1);
+            if (round.getBoard().isOn(diagonal)) {
+                Pair<Piece, Player> diagonalOnBoard = round.getBoard().get(diagonal);
+                if (diagonalOnBoard != null && allegiance != diagonalOnBoard.getRight()) {
+                    moves.add(boardPos.to(diagonal));
+                }
+            }
+        }
+        
+        // En passent (https://en.wikipedia.org/wiki/En_passent)
+        for (int i = -1; i <= 1; i += 2) {
+            // Check for opposite-allegiance pawn at side that just moved a double step
+            Square side = boardPos.withX(boardPos.getX() + i);
+            if (round.getBoard().isOn(side)) {
+                Pair<Piece, Player> sidePiecePlayer = round.getBoard().get(side);
+                if (allegiance != sidePiecePlayer.getRight() && sidePiecePlayer.getLeft() instanceof Pawn
+                        && ((Pawn) sidePiecePlayer.getLeft()).isLastMoveDouble()) {
+                    // Check that the diagonal on that side is clear
+                    Square diagonal = new Square(boardPos.getX() + i, boardPos.getY() + 1);
+                    // round.getBoard().isOn(diagonal) should always be true - simplify???
+                    if (round.getBoard().isOn(diagonal) && round.getBoard().get(diagonal) == null) {
+                        // Use Move subclass to kill side piece when moved
+                        moves.add(new Move(boardPos, diagonal) {
+                            @Override
+                            public void move(@NonNull Board board) {
+                                super.move(board);
+                                board.set(side, null);
+                            }
+                        });
+                    }
+                }
+            }
+        }
+        
+        return Utils.perspectivizeAll(moves, allegiance);
+    }
+    
+    @Override
+    public Pair<String, String> getPackageAndImageName() {
+        // TODO Pawn's icon
+        return Pair.of("ca.keal.sastrane.chess.icon", "TODO_pawn");
+    }
+    
+}
