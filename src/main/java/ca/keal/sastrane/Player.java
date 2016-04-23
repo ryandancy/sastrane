8e097ecb00fe2b6ad3fb76d4f6c485c65e3fb66e
Copper <git@keal.ca>
2016-04-23 06:38:01 -0400

    Changed Player to an interface ("extensible enum").

--- src/main/java/ca/keal/sastrane/Player.java
+++ src/main/java/ca/keal/sastrane/Player.java
@@ -1,16 +1,37 @@
 package ca.keal.sastrane;
 
-import lombok.Data;
+import lombok.NonNull;
 
 /**
- * A general description of a player/team/side/whatever. If you're looking for an AI-or-user input class,
+ * A general description of a player/team/side/whatever. If you're looking for an AI-or-user input type,
  * go to {@link Mover}.
+ * <p />
+ * This interface is designed to be used as an "extensible enum": each implementation of this interface
+ * should be an {@code enum}. For example:
+ * <pre><code>
+ *     public enum FooPlayer implements Player {
+ *         
+ *         PLAYERA("a"), PLAYERB("b");
+ *         
+ *         private String name;
+ *         
+ *         FooPlayer(String name) {
+ *             this.name = name;
+ *         }
+ *         
+ *        {@literal @}Override
+ *         public String getName() {
+ *             return name;
+ *         }
+ *         
+ *     }
+ * </code></pre>
  * 
  * @see Mover
  */
-@Data
-public class Player {
+public interface Player {
     
-    private final String name;
+    @NonNull
+    String getName();
     
 }
