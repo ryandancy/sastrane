95f5d8d73b0ae3bd143ae05f19868e151c93774f
Copper <git@keal.ca>
2016-04-23 08:22:24 -0400

    Added Piece.getPackageAndImageName(); defined how the piece image will be looked for.

--- src/main/java/ca/keal/sastrane/Piece.java
+++ src/main/java/ca/keal/sastrane/Piece.java
@@ -1,10 +1,22 @@
 package ca.keal.sastrane;
 
+import ca.keal.sastrane.util.Pair;
 import lombok.NonNull;
 
 public interface Piece {
     
     @NonNull
     Move[] getPossibleMoves(@NonNull Round round, @NonNull Square boardPos);
+    
+    /**
+     * <strong>Excludes file extension!!!</strong> File extension is assumed to be .png; it + the player's name
+     * will be mangled together to form the final filename. For example a piece that returns {@code ("com.foo", "bar")}
+     * in this method, and whose player's {@link Player#getName()} method returns {@code "baz"}, will have a final image
+     * filename of {@code "com/foo/bar_baz.png"}.
+     * 
+     * @return (packageName, filename) of the piece's icon, with the file name <strong>without the extension!</strong>
+     */
+    @NonNull
+    Pair<String, String> getPackageAndImageName();
     
 }
