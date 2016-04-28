ef2f6c468bae4cefa146e131c92d11ea7a26202b
Copper <git@keal.ca>
2016-04-28 07:06:30 -0400

    Added a title to the new game screen, made it look nice.

--- src/main/java/ca/keal/sastrane/gui/NewGameController.java
+++ src/main/java/ca/keal/sastrane/gui/NewGameController.java
@@ -4,22 +4,23 @@ import ca.keal.sastrane.Game;
 import ca.keal.sastrane.util.Resource;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.SneakyThrows;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 @Getter
 @Setter
-public class NewGameController implements Initializable {
+public class NewGameController {
+    
+    @FXML
+    private Label title;
     
     private Game game;
     
     public void setGame(Game game) {
         this.game = game;
+        this.title.setText("New " + game.getName() + " Game");
     }
     
     @FXML
@@ -27,11 +28,6 @@ public class NewGameController implements Initializable {
     private void onBack(ActionEvent e) {
         // Send back to the main menu
         GuiUtils.getStage(e).setScene(GuiUtils.getScene(new Resource("ca.keal.sastrane.gui", "main-menu.fxml")));
-    }
-    
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        
     }
     
 }
