f7c28c77ae0f5967bf98ec9c85cb734625c5d46d
Copper <git@keal.ca>
2016-04-29 19:52:36 -0400

    Integrated PlayerSettingses with NewGameController.

--- src/main/java/ca/keal/sastrane/gui/NewGameController.java
+++ src/main/java/ca/keal/sastrane/gui/NewGameController.java
@@ -5,22 +5,29 @@ import ca.keal.sastrane.util.Resource;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.SneakyThrows;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
 public class NewGameController {
     
-    @FXML
-    private Label title;
+    @FXML private Label title;
+    @FXML private FlowPane playerSettingsContainer;
     
     private Game game;
     
     public void setGame(Game game) {
         this.game = game;
         this.title.setText("New " + game.getName() + " Game");
+        playerSettingsContainer.getChildren().addAll(Arrays.stream(game.getPlayers())
+                .map(PlayerSettings::new)
+                .collect(Collectors.toList()));
     }
     
     @FXML
@@ -28,6 +35,11 @@ public class NewGameController {
     private void onBack(ActionEvent e) {
         // Send back to the main menu
         GuiUtils.getStage(e).setScene(GuiUtils.getScene(new Resource("ca.keal.sastrane.gui", "main-menu.fxml")));
+    }
+    
+    @FXML
+    private void onCreateGame(ActionEvent e) {
+        // TODO create game
     }
     
 }
