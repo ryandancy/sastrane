9afb8de137c8ecdcf85f492fb7a241dda1075001
Copper <git@keal.ca>
2016-05-10 19:36:26 -0400

    Fixed diagonals.

--- src/main/java/ca/keal/sastrane/tictactoe/TicTacToe.java
+++ src/main/java/ca/keal/sastrane/tictactoe/TicTacToe.java
@@ -35,9 +35,7 @@ public class TicTacToe extends Game {
         BiFunction<Integer, Integer, Square> squareGetter = Square::new;
         for (int maxA = round.getBoard().getMaxX(), maxB = round.getBoard().getMaxY(), i = 0; i < 2; i++) {
             Result result = getStraightLine(maxA, maxB, squareGetter, round);
-            if (result != null) {
-                return result;
-            }
+            if (result != null) return result;
             
             int temp = maxA;
             maxA = maxB;
@@ -45,34 +43,11 @@ public class TicTacToe extends Game {
             squareGetter = (a, b) -> new Square(b, a);
         }
         
-        // diagonal | TODO: reduce repitition here & in getStraightLine
-        Player player = null;
-        boolean won = true;
-        
-        squareGetter = Square::new;
-        for (int i = 0; i < 2; i++) {
-            for (int x = 0, y = 0; x <= round.getBoard().getMaxX() && y <= round.getBoard().getMaxY(); x++, y++) {
-                Square square = squareGetter.apply(x, y);
-                OwnedPiece atSquare = round.getBoard().get(square);
-                if (atSquare == null) {
-                    won = false;
-                    break;
-                }
-        
-                if (player == null) {
-                    player = atSquare.getOwner();
-                } else if (player != atSquare.getOwner()) {
-                    won = false;
-                    break;
-                }
-            }
-    
-            if (won && player != null) {
-                return new Result.Win(player);
-            }
-            
-            squareGetter = (a, b) -> new Square(a - round.getBoard().getMaxX(), b - round.getBoard().getMaxY());
-        }
+        // diagonal
+        Result result = getDiagonal(0, round.getBoard().getMaxX(), +1, round);
+        if (result != null) return result;
+        result = getDiagonal(round.getBoard().getMaxY(), 0, -1, round);
+        if (result != null) return result;
         
         // board filled = draw
         for (Square square : round.getBoard()) {
@@ -81,6 +56,35 @@ public class TicTacToe extends Game {
             }
         }
         return Result.DRAW;
+    }
+    
+    @Nullable
+    private Result getDiagonal(int startX, int endX, int inc, Round round) {
+        // TODO: reduce repitition here & in getStraightLine
+        Player player = null;
+        boolean won = true;
+        
+        for (int x = startX, y = 0; x != endX + inc && y <= round.getBoard().getMaxY(); x += inc, y++) {
+            Square square = new Square(x, y);
+            OwnedPiece atSquare = round.getBoard().get(square);
+            if (atSquare == null) {
+                won = false;
+                break;
+            }
+        
+            if (player == null) {
+                player = atSquare.getOwner();
+            } else if (player != atSquare.getOwner()) {
+                won = false;
+                break;
+            }
+        }
+    
+        if (won && player != null) {
+            return new Result.Win(player);
+        }
+            
+        return null;
     }
     
     @Nullable
