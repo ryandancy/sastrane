500b11e2babebcddb3c232f4165deb1b92ef160d
Copper <git@keal.ca>
2016-05-13 17:20:34 -0400

    Added xiangqi's advisor.

---
+++ src/main/java/ca/keal/sastrane/xiangqi/Advisor.java
@@ -0,0 +1,32 @@
+package ca.keal.sastrane.xiangqi;
+
+import ca.keal.sastrane.api.Player;
+import ca.keal.sastrane.api.Round;
+import ca.keal.sastrane.api.Square;
+import ca.keal.sastrane.api.move.Move;
+import ca.keal.sastrane.api.piece.JumpingPiece;
+import ca.keal.sastrane.util.Resource;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Advisor extends JumpingPiece {
+    
+    public Advisor() {
+        super(1, 1);
+    }
+    
+    @Override
+    public List<Move> getPossibleMoves(Round round, Square boardPos, Player player) {
+        assert player instanceof XiangqiPlayer : "Advisor.getPossibleMoves: !(player instanceof XiangqiPlayer)";
+        return super.getPossibleMoves(round, boardPos, player).stream()
+                .filter(move -> ((XiangqiPlayer) player).getPalace().contains(move.getEndPos()))
+                .collect(Collectors.toList());
+    }
+    
+    @Override
+    public Resource.Unmangled getImage() {
+        return new Resource.Unmangled("ca.keal.sastrane.xiangqi.piece", "advisor.png");
+    }
+    
+}
