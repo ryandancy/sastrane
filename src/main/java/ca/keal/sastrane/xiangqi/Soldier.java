ef0dd568d28c4dd0eafece5196469303ee89bed3
Copper <git@keal.ca>
2016-05-13 19:22:42 -0400

    Added xiangqi's soldier.

---
+++ src/main/java/ca/keal/sastrane/xiangqi/Soldier.java
@@ -0,0 +1,36 @@
+package ca.keal.sastrane.xiangqi;
+
+import ca.keal.sastrane.api.Square;
+import ca.keal.sastrane.api.event.MoveEvent;
+import ca.keal.sastrane.api.move.MovingMove;
+import ca.keal.sastrane.api.piece.JumpingPiece;
+import ca.keal.sastrane.api.piece.OwnedPiece;
+import ca.keal.sastrane.util.Resource;
+import com.google.common.eventbus.Subscribe;
+
+// TODO register
+public class Soldier extends JumpingPiece {
+    
+    public Soldier() {
+        // At start, advance 1
+        super(0, 1, QI | QII);
+    }
+    
+    @Override
+    public Resource.Unmangled getImage() {
+        return new Resource.Unmangled("ca.keal.sastrane.xiangqi.piece", "soldier.png");
+    }
+    
+    @Subscribe
+    public void afterMove(MoveEvent.Post e) {
+        Square endPos = e.getMove().getEndPos();
+        OwnedPiece atEndPos = e.getRound().getBoard().get(endPos);
+        if (atEndPos == null || atEndPos.getPiece() != this) return;
+        
+        if (XiangqiUtils.doesMoveCrossRiver((MovingMove) e.getMove())) {
+            // Soldiers get extra moves after they cross the river - can move to sides
+            addOffsets(1, 0);
+        }
+    }
+    
+}
