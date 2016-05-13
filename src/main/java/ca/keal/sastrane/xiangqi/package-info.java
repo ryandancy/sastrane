17fd96c18befba7ccd13b5aa4c3bd395345ca167
Copper <git@keal.ca>
2016-05-13 18:53:10 -0400

    Added package-info.java to ca.keal.sastrane.xiangqi

---
+++ src/main/java/ca/keal/sastrane/xiangqi/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains the Sastrane implementation of <a href="https://en.wikipedia.org/wiki/Xiangqi">xiangqi</a>.
+ */
+@EverythingIsNonnullByDefault
+package ca.keal.sastrane.xiangqi;
+
+import ca.keal.sastrane.util.EverythingIsNonnullByDefault;
