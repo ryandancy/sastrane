84dd57b0a8bf687520a5c2994fa50b7350a75936
Copper <git@keal.ca>
2016-06-01 07:07:22 -0400

    Fixed Advisor and General's moves
    Advisor and General couldn't move at all. This was because the red and black palaces were mismatched, so each advisor/general thought it was in the other side's palace, leading to it being unable to move.
    
    Fix: swap red and black palaces.

--- src/main/java/ca/keal/sastrane/xiangqi/XiangqiPlayer.java
+++ src/main/java/ca/keal/sastrane/xiangqi/XiangqiPlayer.java
@@ -15,14 +15,14 @@ public enum XiangqiPlayer implements Player {
     
     RED("red", "xiangqi.player.red", new Resource("ca.keal.sastrane.xiangqi.piece", "general_red.png"), Player::flipY,
             ImmutableList.of(
-                    new Square(3, 2), new Square(4, 2), new Square(5, 2),
-                    new Square(3, 1), new Square(4, 1), new Square(5, 1),
-                    new Square(3, 0), new Square(4, 0), new Square(5, 0))),
+                new Square(3, 9), new Square(4, 9), new Square(5, 9),
+                new Square(3, 8), new Square(4, 8), new Square(5, 8),
+                new Square(3, 7), new Square(4, 7), new Square(5, 7))),
     BLACK("black", "xiangqi.player.black", new Resource("ca.keal.sastrane.xiangqi.piece", "general_black.png"),
             Player::flipNone, ImmutableList.of(
-                    new Square(3, 9), new Square(4, 9), new Square(5, 9),
-                    new Square(3, 8), new Square(4, 8), new Square(5, 8),
-                    new Square(3, 7), new Square(4, 7), new Square(5, 7)));
+                new Square(3, 2), new Square(4, 2), new Square(5, 2),
+                new Square(3, 1), new Square(4, 1), new Square(5, 1),
+                new Square(3, 0), new Square(4, 0), new Square(5, 0)));
     
     @Getter private final String name;
     @Getter private final String i18nName;
