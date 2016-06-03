bb622d87a9799c8414897cc4092c8363a84c99c8
Copper <git@keal.ca>
2016-06-03 16:12:09 -0400

    Added Resource.getAllFromFile
    This method parses a mini-language for resource definitions described in its javadoc. It's implemented entirely with Java 8 streams!

--- src/main/java/ca/keal/sastrane/util/Resource.java
+++ src/main/java/ca/keal/sastrane/util/Resource.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.SneakyThrows;
 
 import javax.annotation.Nullable;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URL;
 
 @Data
@@ -36,6 +38,31 @@ public class Resource {
     @Override
     public String toString() {
         return pkg.replace('.', '/') + "/" + filename;
+    }
+    
+    /**
+     * {@code file} should be a text file with newlines separating resources. Each resource in the file is an optional
+     * package (if missing is assumed to be {@code file}'s package), a '/' (forwards slash), and the filename. Comments
+     * are lines <b>starting</b> with '#'. Each line is trimmed.
+     * <p>
+     * e.g., assuming this file is in package {@code com.foo.bar}:
+     * <code><pre>
+     *     # A list of resources
+     *     com.foo.baz/abc.xyz
+     *     asdjke.html
+     * </pre></code>
+     * returns {@code [Resource(pkg=com.foo.baz,filename=abc.xyz),Resource(pkg=com.foo.bar,filename=asdjke.html)]}.
+     */
+    @SneakyThrows
+    public static Resource[] getAllFromFile(Resource file) {
+        return new BufferedReader(new InputStreamReader(file.get().openStream()))
+                .lines()
+                .map(String::trim)
+                .filter(s -> !s.startsWith("#"))
+                .map(s -> s.split("/"))
+                .map(res -> res.length == 2 ? res : new String[] {file.getPkg(), res[1]})
+                .map(res -> new Resource(res[0], res[1]))
+                .toArray(Resource[]::new);
     }
     
     @Data
