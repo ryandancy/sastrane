1d6472e443e0bae7ebe596785572879f73235015
Copper <git@keal.ca>
2016-06-25 11:22:54 -0400

    Added bare minimum go module
    Beginning to add the game of go: added the module (not wired up in Main, however), along with css, icon, and i18n.

---
+++ src/main/java/ca/keal/sastrane/go/GoModule.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2016 Ryan Dancy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations under the License.
+ */
+
+package ca.keal.sastrane.go;
+
+import ca.keal.sastrane.api.AbstractGameModule;
+import ca.keal.sastrane.api.GameAttr;
+
+public class GoModule extends AbstractGameModule {
+    
+    public GoModule() {
+        super("ca.keal.sastrane.go");
+    }
+    
+    @Override
+    public void configure() {
+        bindToInstance(GameAttr.NAME, String.class, "go");
+        bindToInstance(GameAttr.PACKAGE, String.class, "ca.keal.sastrane.go");
+        bindToInstance(GameAttr.ALLOW_PASSING, Boolean.class, true);
+        
+        super.configure();
+    }
+    
+}
