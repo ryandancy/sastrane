ebc8fd759a6d5afdb8c3b4e80f87570dcf8c4dc0
Copper <git@keal.ca>
2016-06-26 08:04:38 -0400

    Add Chain, representing a chain of stones
    Chain represents an unbroken chain of stones; that is, a number of
    stones where a path can be drawn through all the stones in the chain
    crossing over only stones of the same colour.
    
    Chain is little more than a glorified List<Stone>. Eventually, it will have
    more instance method, but currently it's merely a wrapper over its
    List<Stone>.
    
    However, Chain's static methods, `at` and `getAllOfPlayer`, are quite
    complex. `at` attempts to find a chain at the specified square, and
    does this by recursively adding adjacent stones. `getAllOfPlayer`
    attempts to find *all* chains of the specified player's colour, and
    does this by using `at`, along with some fancy checking to make sure
    there are no duplicate chains, etc. `at` returns a single Chain, while
    `getAllOfPlayer` returns a List<Chain>.
    
    Chain will, in a more evolved form, be used in GoMove to
    determine what to remove.

---
+++ src/main/java/ca/keal/sastrane/go/Chain.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2016 Ryan Dancy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations under the License.
+ */
+
+package ca.keal.sastrane.go;
+
+import ca.keal.sastrane.api.Board;
+import ca.keal.sastrane.api.Player;
+import ca.keal.sastrane.api.Square;
+import ca.keal.sastrane.api.piece.OwnedPiece;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents an unbroken group of like-coloured stones.
+ */
+@RequiredArgsConstructor
+class Chain {
+    
+    @Getter private final List<Stone> stones;
+    
+    boolean isEmpty() {
+        return stones.isEmpty();
+    }
+    
+    Player getPlayer() {
+        if (isEmpty()) throw new IllegalStateException("Empty chain has no player");
+        return stones.get(0).getPlayer();
+    }
+    
+    /**
+     * Get a list of all chains of {@code player}'s color anywhere on the board.
+     */
+    static List<Chain> getAllOfPlayer(Player player, Board board) {
+        List<Chain> chains = new ArrayList<>();
+        for (Square square : board) {
+            OwnedPiece atSquare = board.get(square);
+            if (atSquare == null || atSquare.getOwner() != player) {
+                // It's not a stone of our colour
+                continue;
+            }
+            
+            Stone stone = (Stone) atSquare.getPiece();
+            if (chains.stream().anyMatch(c -> c.getStones().contains(stone))) {
+                // It's a duplicate chain
+                continue;
+            }
+            
+            Chain chain = at(square, board);
+            if (!chain.isEmpty()) {
+                chains.add(chain);
+            }
+        }
+        return chains;
+    }
+    
+    /**
+     * Attempt to find a chain containing {@code square} on {@code board}, and returns an empty chain if {@code square}
+     * is empty.
+     */
+    static Chain at(Square square, Board board) {
+        OwnedPiece piece = board.get(square);
+        // Return null if piece == null for better performance???
+        if (piece == null) return new Chain(new ArrayList<>()); // square is empty
+        
+        Stone stone = (Stone) piece.getPiece();
+        
+        List<Stone> stones = new ArrayList<>();
+        addStonesInChain(stones, stone, board);
+        
+        return new Chain(stones);
+    }
+    
+    /** Recursively adds all stones part of the chain. */
+    private static void addStonesInChain(List<Stone> stones, Stone start, Board board) {
+        stones.add(start);
+        for (Square adj : start.getAdjacent(board)) {
+            OwnedPiece piece = board.get(adj);
+            if (piece == null) continue; // empty point
+            
+            Stone stone = (Stone) piece.getPiece();
+            if (stone.getPlayer() != start.getPlayer()) continue; // must be same colour
+            if (stones.contains(stone)) continue; // don't call this method for the same stone twice
+            
+            addStonesInChain(stones, stone, board);
+        }
+    }
+    
+}
