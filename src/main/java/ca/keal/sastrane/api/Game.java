416659f7e16e130bbb4f63de35e6d74562f4d6d8
Copper <git@keal.ca>
2016-09-04 08:10:52 -0400

    Add Game class
    ~2 months later, I realized how mangled the DI usage is. So, we're
    going back to the original method (kinda). One object, a Game class,
    which is DI'd into anything that needs info about the game. Kapich?
    
    Each game will provide an implementation of Game, and will bind that in
    its module, instead of individual stuff. Eventually, GameAttr and the
    like will be completely removed, and it'll be testable. Probably.
    Hopefully. In all honesty, probably not.

---
+++ src/main/java/ca/keal/sastrane/api/Game.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2016 Ryan Dancy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations under the License.
+ */
+
+package ca.keal.sastrane.api;
+
+import ca.keal.sastrane.api.piece.PlacingPiece;
+import ca.keal.sastrane.util.Resource;
+import com.google.common.eventbus.EventBus;
+
+import javax.annotation.Nullable;
+import java.util.function.Consumer;
+
+public abstract class Game {
+    
+    public abstract String getName();
+    
+    public abstract String getPackage();
+    
+    public String getResourceBundleName() {
+        return getName() + ".i18n." + getPackage();
+    }
+    
+    public String getI18nName() {
+        return getName() + ".name";
+    }
+    
+    public Resource getIcon() {
+        return new Resource(getPackage(), getName() + ".png");
+    }
+    
+    public Resource getCss() {
+        return new Resource(getPackage(), getName() + ".css");
+    }
+    
+    public BoardDecor[] getBoardDecor() {
+        return new BoardDecor[0];
+    }
+    
+    public abstract Player[] getPlayers();
+    
+    public abstract AI getAI();
+    
+    public abstract Board.Factory getBoardFactory();
+    
+    public PlacingPiece[] getPlacingPieces() {
+        return new PlacingPiece[0];
+    }
+    
+    /** Override this to return false if your game has both placing and non-placing pieces */
+    public boolean isPlaceOnly() {
+        return getPlacingPieces().length > 0;
+    }
+    
+    public abstract Arbitrator getArbitrator();
+    
+    /** null -> not notatable */
+    @Nullable
+    public Notater getNotater() {
+        return null;
+    }
+    
+    // TODO remove the "defaults registrator" stuff
+    public Consumer<EventBus> getDefaultsRegistrator() {
+        return bus -> {};
+    }
+    
+    public boolean allowPassing() {
+        return false;
+    }
+    
+    public boolean isAutoPassingEnabled() {
+        return false;
+    }
+    
+}
