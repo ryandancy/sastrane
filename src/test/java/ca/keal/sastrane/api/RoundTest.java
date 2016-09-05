5f0e52ff4a00fa8cd0b6827528abbf891d31016f
Copper <git@keal.ca>
2016-09-05 06:03:40 -0400

    Fix/Update RoundTest, remove TestUtils
    Updated RoundTest to the new system (the project compiles now), and
    it's SO much easier to do stuff. So much easier, in fact, that
    TestUtils isn't even needed! That's been removed.

--- src/test/java/ca/keal/sastrane/api/RoundTest.java
+++ src/test/java/ca/keal/sastrane/api/RoundTest.java
@@ -16,16 +16,12 @@ package ca.keal.sastrane.api;
 import ca.keal.sastrane.api.move.MovingMove;
 import ca.keal.sastrane.api.piece.MovingPiece;
 import ca.keal.sastrane.api.piece.OwnedPieceFactory;
-import ca.keal.sastrane.api.piece.PlacingPiece;
-import ca.keal.sastrane.test.TestUtils;
 import com.google.common.eventbus.EventBus;
-import lombok.val;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import javax.annotation.Nullable;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -46,22 +42,11 @@ public class RoundTest {
     @Mock private Mover mover1, mover2;
     private Map<Player, Mover> playersToMovers;
     
-    private final String name = TestUtils.TEST_GAMEID;
-    
     @Mock private Board board;
     @Mock private Board.Factory boardFactory;
     
-    private final Consumer<EventBus> defaultsRegistrator = e -> {};
-    private Player[] players;
-    
-    @Mock private PlacingPiece piece;
-    private PlacingPiece[] placingPieces;
-    
     @Mock private Arbitrator arbitrator;
     @Mock private Notater notater;
-    private Boolean autoPassEnabled;
-    
-    private Round round;
     
     @BeforeMethod(alwaysRun = true)
     public void before() {
@@ -73,72 +58,47 @@ public class RoundTest {
         
         when(boardFactory.bus(any())).thenReturn(boardFactory);
         when(boardFactory.build()).thenReturn(board);
-        
-        players = new Player[] {player1, player2};
-        placingPieces = new PlacingPiece[] {piece};
-        autoPassEnabled = false;
-        
-        round = new Round(
-                TestUtils.TEST_GAMEID, playersToMovers, TestUtils.getInjectedMapAsTest(name),
-                TestUtils.getInjectedMapAsTest(boardFactory), TestUtils.getInjectedMapAsTest(defaultsRegistrator),
-                TestUtils.getInjectedMapAsTest(players), TestUtils.getInjectedMapAsTest(placingPieces),
-                TestUtils.getInjectedMapAsTest(arbitrator), TestUtils.getInjectedMapAsTest(notater),
-                TestUtils.getInjectedMapAsTest(autoPassEnabled));
-    }
-    
-    @SuppressWarnings("ConstantConditions")
-    private Round getRoundWith(@Nullable String gameID, @Nullable Map<Player, Mover> playersToMovers,
-                               @Nullable String name, @Nullable Board.Factory boardFactory,
-                               @Nullable Consumer<EventBus> defaultsRegistrator, @Nullable Player[] players,
-                               @Nullable PlacingPiece[] placingPieces, @Nullable Arbitrator arbitrator,
-                               @Nullable Notater notater, @Nullable Boolean autoPassEnabled) {
-        if (gameID == null) gameID = TestUtils.TEST_GAMEID;
-        if (playersToMovers == null) playersToMovers = this.playersToMovers;
-        if (name == null) name = this.name;
-        if (boardFactory == null) boardFactory = this.boardFactory;
-        if (defaultsRegistrator == null) defaultsRegistrator = this.defaultsRegistrator;
-        if (players == null) players = this.players;
-        if (placingPieces == null) placingPieces = this.placingPieces;
-        if (arbitrator == null) arbitrator = this.arbitrator;
-        if (notater == null) notater = this.notater;
-        if (autoPassEnabled == null) autoPassEnabled = this.autoPassEnabled;
-        
-        return new Round(
-            gameID, playersToMovers, TestUtils.getInjectedMapAsTest(name),
-            TestUtils.getInjectedMapAsTest(boardFactory), TestUtils.getInjectedMapAsTest(defaultsRegistrator),
-            TestUtils.getInjectedMapAsTest(players), TestUtils.getInjectedMapAsTest(placingPieces),
-            TestUtils.getInjectedMapAsTest(arbitrator), TestUtils.getInjectedMapAsTest(notater),
-            TestUtils.getInjectedMapAsTest(autoPassEnabled));
     }
     
     // Constructor
     
-    public void constructor_withGameID_setsGameID() {
-        assertEquals(round.getGameID(), TestUtils.TEST_GAMEID);
+    public void constructor_withGame_setsGame() {
+        Game game = mock(Game.class);
+        Round round = new Round(game, playersToMovers);
+        assertEquals(round.getGame(), game);
     }
     
     public void constructor_withPlayersToMovers_setsPlayersToMovers() {
+        Round round = new Round(mock(Game.class), playersToMovers);
         assertEquals(round.getPlayersToMovers(), playersToMovers);
     }
     
     public void constructor_withBoardFactory_setsBoardBuiltByFactory() {
+        Game game = mock(Game.class);
+        when(game.getBoardFactory()).thenReturn(boardFactory);
+        Round round = new Round(game, playersToMovers);
         assertEquals(round.getBoard(), board);
     }
     
     public void constructor_withName_setsEventBusWithName() {
+        String name = "__TEST__";
+        Game game = mock(Game.class);
+        when(game.getName()).thenReturn(name);
+        Round round = new Round(game, playersToMovers);
         assertEquals(round.getBus().identifier(), name);
     }
     
-    @SuppressWarnings("unchecked")
-    public void constructor_withDefaultsRegistrator_callsIt() {
-        Consumer<EventBus> defaultsRegistrator = mock(Consumer.class);
-        getRoundWith(null, null, null, null, defaultsRegistrator, null, null, null, null, null);
+    public void constructor_withDefaultsRegistrator_callsItOnce() {
+        Game game = mock(Game.class);
+        Consumer<EventBus> defaultsRegistrator = bus -> {};
+        when(game.getDefaultsRegistrator()).thenReturn(defaultsRegistrator);
         verify(defaultsRegistrator, times(1)).accept(any());
     }
     
     // Copy constructor
     
     public void copyConstructor_copies() {
+        Round round = new Round(mock(Game.class), playersToMovers);
         Round copy = new Round(round);
         assertFalse(round == copy); // not returning the same object
         assertEquals(round, copy);
@@ -151,11 +111,12 @@ public class RoundTest {
         when(piece.getPossibleMoves(any(), any(), any()))
                 .thenReturn(Collections.singletonList(new MovingMove(new Square(0, 0), new Square(1, 0))));
         
-        val round = getRoundWith(null, null, null, Board.factory()
-                    .row("a ")
-                    .piece('a', new OwnedPieceFactory(() -> piece, player1)),
-                null, null, null, null, null, false);
+        Game game = mock(Game.class);
+        when(game.getBoardFactory()).thenReturn(Board.factory()
+                .row("a ")
+                .piece('a', new OwnedPieceFactory(() -> piece, player1)));
         
+        Round round = new Round(game, playersToMovers);
         assertFalse(round.willAutoPass(player1));
     }
     
